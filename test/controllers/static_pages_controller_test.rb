e5582b46a7be12cebc1660390185c66cad691f99
Emily Cramer <ecramer89@gmail.com>
2015-10-10 23:44:54 +0000

    finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,20 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Welcome To The Page For | HOME"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+   assert_select "title", "Welcome To The Page For | HELP"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "Welcome To The Page For | ABOUT"
+  
   end
 
 end
